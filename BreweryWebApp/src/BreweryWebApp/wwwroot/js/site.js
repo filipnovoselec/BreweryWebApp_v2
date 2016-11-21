25543ae78b2e14e8eef116f4a10393d3d4d82e0b
Filip Novoselec <filip.novoselec@gmail.com>
2016-11-21 22:45:26 +0100

    wip

--- BreweryWebApp/src/BreweryWebApp/wwwroot/js/site.js
+++ BreweryWebApp/src/BreweryWebApp/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿// Write your Javascript code.
 var BreweryApp = angular.module('BreweryApp',
-['ui.router', 'ngAnimate', 'ngRoute', 'chart.js', 'stormpath', 'stormpath.templates']);
+['ui.router', 'ngAnimate', 'stormpath']);
 
 BreweryApp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
@@ -9,31 +9,31 @@ BreweryApp.config(function($stateProvider, $urlRouterProvider) {
         .state('home',
         {
             url: '/',
-            templateUrl: '~/templates/homePage.html',
+            templateUrl: '/templates/homePage.html',
             controller: 'homepageController'
         })
         .state('login',
         {
             url: '/login',
-            templateUrl: '~/templates/loginPage.html',
+            templateUrl: '/templates/loginPage.html',
             controller: 'loginController'
         })
         //.state('recipes',
         //{
         //    url: '/recipes',
-        //    templateUrl: '~/templates/recipesPage.html',
+        //    templateUrl: '/templates/recipesPage.html',
         //    controller: 'recipesController'
         //})
         .state('recipe',
         {
             url: '/recipeDetails/{name}',
-            templateUrl: '~/templates/recipeDetailsPage.html',
+            templateUrl: '/templates/recipeDetailsPage.html',
             controller: 'recipeDetailsController'
         })
         .state('beer',
         {
             url: '/beer',
-            templateUrl: '~/templates/beerPage.html',
+            templateUrl: '/templates/beerPage.html',
             controller: 'beerController'
         });
 })
