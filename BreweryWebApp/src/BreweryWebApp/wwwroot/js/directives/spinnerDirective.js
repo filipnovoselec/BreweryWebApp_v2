33971e01111c49600c7877df9867d27d1ad9e243
Filip Novoselec <filip.novoselec@gmail.com>
2016-11-23 22:49:53 +0100

    Directive creation

---
+++ BreweryWebApp/src/BreweryWebApp/wwwroot/js/directives/spinnerDirective.js
@@ -0,0 +1,27 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('spinnerDirective', spinnerDirective);
+
+    spinnerDirective.$inject = ['$window'];
+    
+    function spinnerDirective ($window) {
+        // Usage:
+        //     <spinnerDirective></spinnerDirective>
+        // Creates:
+        // 
+        var directive = {
+            link: link,
+            restrict: 'EA',
+            replace: true,
+            template: '/templates/spinnerTemplate.html'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+        }
+    }
+
+})();
