c2260c3ed13f88e80941d8c8dd5878eb10ed2e8f
Filip Novoselec <filip.novoselec@gmail.com>
2016-11-27 19:25:21 +0100

    WIP

---
+++ BreweryWebApp/BreweryData/Models/Beers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreweryData.Models
+{
+    class Beers
+    {
+        public Beers()
+        {
+            this.Pump = new HashSet<Pump>();
+            this.Temperatures = new HashSet<Temperatures>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Rating { get; set; }
+        public string Picture { get; set; }
+
+        public virtual ICollection<Pump> Pump { get; set; }
+        public virtual ICollection<Temperatures> Temperatures { get; set; }
+    }
+}
