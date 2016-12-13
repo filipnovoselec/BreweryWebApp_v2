93b9d3d8c06fe0b5d7f78dc67aba9c5093394e0e
Filip Novoselec <filip.novoselec@gmail.com>
2016-12-13 22:27:27 +0100

    Entity model fix

--- BreweryWebApp/BreweryData/Models/Temperatures.cs
+++ BreweryWebApp/BreweryData/Models/Temperatures.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace BreweryData.Models
     public partial class Temperatures
     {
         public int Id { get; set; }
-        public float Temp { get; set; }
+        public double Temp { get; set; }
         public DateTime Time { get; set; }
-        
+        public int BeerId { get; set; }
+
+        [ForeignKey("BeerId")]
         public virtual Beers Beer { get; set; }
     }
 }
